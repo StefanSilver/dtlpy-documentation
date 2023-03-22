64e442de7dfa6e292a8fbc9e1b2b39694a1a9e00
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-03-22 20:44:09 +0200

    Minor Changes.

--- docs_build/tutorials_templates/faas/execution_control/mds.py
+++ docs_build/tutorials_templates/faas/execution_control/mds.py
@@ -2,33 +2,31 @@ def func1():
     """
     # Executions Control
     ## Execution Termination
-    Sometimes when we run long term executions, such as model training, we need the option to terminate the execution. This is facilitated using terminate at Checkpoint.
-    To stop an execution set the code checkpoints to check if this execution received a termination and if it did, raise the Termination Exception.
-    This allows you to save some work that was already done before terminating.
-    For example:
+    Sometimes when we need to run long Executions of various Services, such as model training. That's why we need to have the option to terminate an Execution. This is facilitated using the Terminate at Checkpoint.
+   
+    To stop an Execution set the code checkpoints to check if this Execution received a termination; if it did, raise the Termination Exception.
+    This allows you to save the processing that was already done before terminating the execution. Here is an example:
     """
 
 
 def func2():
     """
-    Each time there is a "kill_event" the service runner checks to see if this execution received a termination request.
-    To kill such execution we use
+    Each time there is a "kill_event" the Service runner checks to see if this Execution received a termination request. To kill such Execution we use the following command:
     """
 
 
 def func3():
     """
     ## Execution Timeout
-    You can tell an execution to stop after a given number of seconds with the timeout parameter (the default time is 1 hour).
-    In case a service reset, such as in timeout or service update, If there are running executions the service will wait for the execution timeout before resetting.
-    The number have to be a natural number (int).
+    You can tell an Execution to stop after a given number of seconds with the timeout parameter (the default time is 1 hour).
+    In case a Service reset, such as in timeout or Service update, if there are running Executions the service will wait for the Execution timeout before resetting. The value needs to be a natural number (int):
     """
 
 
 def func4():
     """
-    You can decide what to do to executions that have experienced a timeout. There are 2 options of timeout handling:
-    1. Mark execution as failed
-    2. Retry
+    You can also decide what to do to Executions that have experienced a timeout. There are 2 options of timeout handling:
+    1. Mark Execution as failed;
+    2. Retry (rerun) Execution.
     """
 
