c23f9ba86710864df000bb98f51581913e8a1a5c
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-03-22 20:51:49 +0200

    Minor changes

--- docs_build/tutorials_templates/faas/custom_environment_using_docker/mds.py
+++ docs_build/tutorials_templates/faas/custom_environment_using_docker/mds.py
@@ -2,15 +2,13 @@ def func1():
     """
     # FaaS Docker Image
 
-    Dataloop enables you to deploy in the FaaS module a custom docker image, to enrich your application with literally
-    anything required for your project. Deploying a docker image is as easy as providing the Docker image path when
-    deploying the service:
+    Dataloop enables you to deploy a custom Docker image in the FaaS module , to enrich your application with literally anything required for your Project. Deploying a Docker image is as easy as providing the Docker image path when deploying the Service:
     """
 
 
 def func2():
     """
-    or if you want to update an existing service:
+    In some cases, you may only want to  update an existing Service. To do that, you can use the code below:
     """
 
 
@@ -19,24 +17,22 @@ def func3():
 
     ## Our Docker Image
 
-    We have our list of docker images publicly available in [Dockerhub](https://hub.docker.com/repository/registry-1.docker.io/dataloopai/dtlpy-agent/tags)
-    You can see the env of each docker on the dockerfile [here](https://github.com/dataloop-ai/dtlpy-agent/tree/main/dockerfiles)
+    We have our list of Docker images publicly available in [Dockerhub](https://hub.docker.com/repository/registry-1.docker.io/dataloopai/dtlpy-agent/tags).
 
     ## Public Docker Images
 
-    You can use any public docker image, and on runtime, the Dataloop agent will install:
+    You can use any public Docker image, and the Dataloop agent will install the following at runtime:
 
-    1. Package requirements
-    2. dtlpy package (version as defined on the service)
-    3. dtlpy-agent (same version as the SDK)
+    1. Package requirements;
+    2. dtlpy Package (version as defined on the Service);
+    3. dtlpy-agent (same version as the SDK).
 
     For example, using `docker.io/python:3.9.13` will run the function with Python 3.9.
 
     ## Build Your Own Docker Image
 
-    If you want other environment or need to add some apt-get installation, you can create any docker image and use it directly.
-    You will need to set the HOME directory to `/tmp` and install the python packages with --user (or as USER 1000).
-    For instance:
+    If you want another environment or need to add some apt-get installation, you can create any Docker image and use it directly.
+    You will need to set the HOME directory to `/tmp` and install the python packages with `--user` (or as USER 1000). For example:
     ```
     FROM dockerhub.io/dataloopai/dtlpy-agent:latest.gpu.cuda11.5.py3.8.opencv
 
@@ -55,6 +51,6 @@ def func3():
 
     ## Using Private Docker Registry
 
-    To connect a private registry, you'll need to add the docker container registry credentials as an Organization Secret (ONLY in the UI) and just create use the runner image.
+    To connect a private registry, you'll need to add the Docker container registry credentials as an Organization Secret (ONLY in the UI version) and just use the runner image.
 
     """
