6e6136efe492ddd4ff0bf25d413ce6eb21d8b988
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-03-22 13:31:52 +0200

    Minor changes and improvements + few links

--- docs_build/tutorials_templates/faas/introduction/mds.py
+++ docs_build/tutorials_templates/faas/introduction/mds.py
@@ -1,30 +1,33 @@
 def func1():
     """
     # Introduction
-    This tutorial will help you get started with FaaS.
-    1. Prerequisites
-    2. Basic use case:  Single function
-    * Deploy a function as a service
-    * Execute the service manually and view the output
-    3. Advance use case: Multiple functions
-    * Deploy several functions as a package
-    * Deploy a service of the package
-    * Set trigger events to the functions
-    * Execute the functions and view the output and logs
-
-    First, log in to the platform by running the following Python code in the terminal or your IDE: 
+    This tutorial will help you get started with using Dataloop's FaaS. Here are some of the things you will learn:
+    
+    1. Prerequisites;
+    2. Basic use case:  Single function;
+    * Deploy a function as a service;
+    * Execute the service manually and view the output;
+    3. Advance use case: Multiple functions;
+    * Deploy several functions as a package;
+    * Deploy a service of the package;
+    * Set trigger events to the functions;
+    * Execute the functions and view the output and logs.
+    
+    To get started with everything, you must first make sure you have the Dataloop Python SDK installed. If you don't, please refer to [Chapter 1 of Dataloop's Python SDK Onboarding](https://github.com/dataloop-ai/dtlpy-documentation/blob/main/onboarding/01_python_sdk_installation.md), to learn how to do it.
+    
+    If you have Dataloop's Python SDK installed, you can proceed to learn how to use our FaaS. You need to first log in to the platform by running the following Python code in the terminal or your IDE: 
     """
 
 
 def func2():
     """
-    Your browser will open a login screen, allowing you to enter your credentials or log in with Google. Once the "Login Successful" tab appears, you are allowed to close it.
+    Your browser will open a login screen, allowing you to enter your credentials or log in with Google. Once the "Login Successful" tab appears, you can to close itand return to your console or IDE.
 
-    This tutorial requires a project. You can create a new project, or alternatively use an existing one: 
+    This tutorial requires you to have already created a Project. You can create a new project ([click here to learn more about Projects](https://github.com/dataloop-ai/dtlpy-documentation/blob/main/onboarding/02_login_and_project_and_dataset_creation.md)), or alternatively use an existing one: 
     """
 
 
 def func3():
     """
-    Let’s create a dataset to work with and upload a sample item to it:
+    You will also need to create a Dataset and add at least an Item to it. The code below creates a Dataset and uploads a sample Item to it (you will need to add your own Item, and change the `local_path` to where that Item is placed on your computer):
     """
