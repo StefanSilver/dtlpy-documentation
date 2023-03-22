b833507004e137cdd448b69b970c05e4a11d98c3
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-03-22 20:18:20 +0200

    Minor edits

--- docs_build/tutorials_templates/faas/advance/mds.py
+++ docs_build/tutorials_templates/faas/advance/mds.py
@@ -1,24 +1,26 @@
 def func1():
     """
-    # Advance Configurations
+    # Advanced Configurations
     ## Service Runtime
+    Services are configured with a runtime driver and runtime configuration, which defines how the Service should be deployed. Currently, the only supported driver is the Kubernetes driver.
+    When a Service is deployed with the Kubernetes driver, a Kubernetes deployment is created. A Kubernetes deployment basically tells Kubernetes how many replicas of the Service should be up at any point in time, and how many resources should be allocated to it.
     """
 def func2():
     """
     ## Autoscaler
-    When we have changing loads of work, we want the number of replicas of the service to scale up when there are many executions coming in, and scale down otherwise.
-    To do so, we need to create an autoscaler:
+    When we have varying loads of work, we want the number of service replicas to scale up when there are a lot of executions coming in and scale down otherwise.
+    To do so, we need to create an Autoscaler, as using the code below:
     """
 
 def func3():
     """
     ## Using Secrets in a Function
-    You can use organization integration secrets (key-value) as environment variable inside the function's env.
-    First you'll need to add the integrations (UI only), then simply add the integrations' ids whe deploying the service:
+    You can use Organization integration Secrets (key-value) as environment variable inside the function's environment.
+    First you'll need to add the integrations (possible only in UI mode), then simply add the integrations' IDs when deploying the Service:
     """
 
 
 def func4():
     """
-    Inside the service you can access the values using os package:
+    Inside the Service you can access the values using an OS Package, as seen below:
     """
