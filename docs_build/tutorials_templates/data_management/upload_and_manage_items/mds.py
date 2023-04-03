5d98af1805bf635ff81253ebd88b19b0bfc89dad
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-03 15:43:07 +0300

    Update mds.py

--- docs_build/tutorials_templates/data_management/upload_and_manage_items/mds.py
+++ docs_build/tutorials_templates/data_management/upload_and_manage_items/mds.py
@@ -1,11 +1,13 @@
 def section1():
     """
     # Upload & Manage Data & Metadata
-
+    Data samples you upload to your Dataset will be stored as something called an Item. Items are Dataloop objects (or entities) belonging to the Dataloop Item class. They are represented as a Dataloop data model object and are stored as a binary object along with a JSON file that contains Metadata about the Item. Item JSON files also contain Metadata from Annotations, for example, that you add to further describe or classify the data sample. Each Item has a stream method to get the data sample along with other attributes, such as the Metadata, from the accompanying JSON file.
     ## Upload Specific Files
 
-    When you have specific files you want to upload, you can upload them all into a dataset using this script:
-"""
+    When you have specific files you want to upload, you can upload them all into a Dataset. You can upload (multiple) single Items using the script below:
+    **Note:** If you are on Windows, you will have to add an 'r' before the path to the location of the data you want to upload - if you are on Linus or Mac, that 'r' need to be removed.
+    
+    """
 
 
 def section2():
@@ -15,7 +17,7 @@ def section2():
     ## Upload All Files in a Folder
 
 
-    If you want to upload all files from a folder, you can do that by just specifying the folder name:
+    If you want to upload all files from a folder, you can do that in a similar way to uploading single Items. However, here you have to specify the folder name and the `local_path` to that folder, as you can see below:
 
     """
 
@@ -24,14 +26,15 @@ def section3():
     """
 
     ## Upload Items From URL Links
-    You can provide Dataloop with the link to the item, and not necessarily the item itself.
+    You can also provide Dataloop with the link to an Item, if you choose to upload data from the web:
     """
 
 
 def section4():
     """
 
-    You can open an item uploaded to Dataloop by opening it in a viewer.
+    You can visualize an Item uploaded to Dataloop by opening it in the WebUI version of Dataloop. This will open a new tab in which the Item will be shown. To do that, use the simple line of code below:
+    **Note:** The `.open_in_web()` method can be used for all kinds of Dataloop entities, such as Projects (`poject.open_in_web()`)
     """
 
 
