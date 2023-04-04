b9dbd09d65e8ec7f34950c832a3bbda8df82a45c
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-04 20:35:02 +0300

    Update mds.py

--- docs_build/tutorials_templates/data_management/sort_and_filter/advanced_sdk_filters/mds.py
+++ docs_build/tutorials_templates/data_management/sort_and_filter/advanced_sdk_filters/mds.py
@@ -1,101 +1,98 @@
 def section1():
     """
-    ## Advance SDK Filters
-    To access the filters entity click <a href="https://sdk-docs.dataloop.ai/en/latest/entities.html#module-dtlpy.entities.filters" target="_blank">here</a>.
-    ### Filter Operators
-    To understand more about filter operators please click <a href="https://dataloop.ai/docs/dql-operators" target="_blank">here</a>.
+    # Advanced Filters in the Python SDK
+    Filters are part of the Dataset and Task Browsers, enabling you to Filter Items based on every aspect of your files. When multiple Filters are used, the relationship between them will be the AND logical operator. However, the relationship between multiple values in each Filter will be the OR logical operator. For example, entering "dog" and "cat" in the Labels Filter will result in all Items that have a "dog" Label OR a "cat" Label.
+    Here are some resources that will help you learn more about Filters:
+    - [Beginner Filter Onboarding](https://github.com/dataloop-ai/dtlpy-documentation/blob/main/onboarding/04_metadata_querying_and_filtering.md).
+    - [Dataloop's Query Language Documentation](https://dataloop.ai/docs/api-dql).
+    - [General Filters Documenation](https://dataloop.ai/docs/sort-filter).
+    - [DQL Tutorial](https://github.com/dataloop-ai/dtlpy-documentation/blob/main/tutorials/data_management/sort_and_filter/annotation_level/chapter.md/)
+    
+    ## Filter Operators
 
-    When adding a filter, several operators are available for use:
+    When adding a Filter, There are several operators you could use. In this section, we will explore the most important ones and provide what you can
 
-    #### Equal
-    eq -> equal
-    (or dl.FiltersOperation.EQUAL)
+    The **Equal** or `eq` Operator (dl.FiltersOperation.EQUAL) filters for items that are equal to the parameter you filter by.
+    
 
-    For example, filter items from a specific folder directory.
+    In the example below, we Filter Items from a specific folder directory.
     """
 
 
 def section2():
     """
-    #### Not Equal
+    The ***Not Equal*** or `ne` Operator (dl.FiltersOperation.NOT_EQUAL)
     ne -> not equal
-    (or dl.FiltersOperation.NOT_EQUAL)
+    
 
-    In this example, you will get all items that do not have ONLY a 'cat' label.
-    **Note**
-    This Operator is a better fit for filters of a single value because, for example, this filter will return items that have both 'cat' and 'dog' labels.
-    View an example of the solution [here](#how-to-filter-items-that-dont-have-a-specific-label).
+    In the example below, you will get all Items that do have ONLY a 'cat' Label.
+    **Note:** This Operator is a better fit for filters of a single value.
     """
 
 
 def section3():
     """
-    #### Greater Than
-    gt -> greater than
-    (or dl.FiltersOperation.GREATER_THAN)
+    The ***Greater Than*** Operator or `gt` (dl.FiltersOperation.GREATER_THAN) searches for Items that are above the given threshold you set.
 
-    You will get items with a greater height (in pixels) than the given value in this example.
+
+    In the example below, the script gets all Items with a greater height (in pixels) than the given threshold value.
     """
 
 
 def section4():
     """
-    #### Less Than
-    lt -> less than
-    (or dl.FiltersOperation.LESS_THAN)
+    The ***Less Than*** Operator or `lt` (dl.FiltersOperation.LESS_THAN) is the oposite of the Greater Than Operator, meaning that it will search for Items that are below a given threshold value.
 
-    You will get items with a width (in pixels) less than the given value in this example.
+
+   In the example below, the script gets all Items with a width (in pixels) that isless than the given threshold value.
     """
 
 
 def section5():
     """
-    #### In a List
-    in -> is in a list (when using this expression, values should be a list).
-    (or dl.FiltersOperation.IN)
-    In this example, you will get items with dog OR cat labels.
+    The ***In a List*** Operator or `in`(dl.FiltersOperation.IN), is a `list` type, where you can provide multiple Labels or other parameters that the Filter will look for.
+
+    In the example below, the script will get all Items with the dog OR cat Labels.
     """
 
 
 def section6():
     """
     ### SDK defaults
-    Filters ignore SDK defaults like hidden items and directories or note annotations as issues.
-    If you wish to change this behavior, you may do the following:
+    Filters ignore SDK defaults like hidden Items and directories or note Annotations as issues. If you wish to change this behavior, you can use the following script:
     """
 
 
 def section7():
     """
-    #### Hidden Items and Directories
-    If you wish to only show hidden items & directories in your filters use this code:
+    ### Hidden Items and Directories
+    If you wish to only show hidden Items & directories in your Filters use this code:
     """
 
 
 def section8():
     """
     ### Delete a Filter
+    Sometimes you may wish to delete a Filter that is stored in Dataloop. To do that, use the script below.
     """
 
 
 def section9():
     """
-    ### Full Examples
-    #### How to filter items that were created between specific dates?
-    In this example, you will get all of the items that were created in 2018.
+    ## Other Examples
+    ### Filter forItems that were created between specific dates
+    In this example, you will get all of the Items that were created in 2018. You can adapt it to your needs, so you can Filter for Items created between any dates you wish.
     """
 
 
 def section10():
     """
-    #### How to filter items that don't have a specific label?
-    In this example, you will get all items that do not have a 'cat' label AT ALL.
-    <div style="background-color: lightblue; color: black; width: 50%; padding: 10px; border-radius: 15px 5px 5px 5px;"><b>Note</b><br>
-    This filter will NOT return items that have both 'cat' and 'dog' labels.</div>
+    ### Filter for Items that don't have a specific Label
+    In this example, you will get all Items that **do not have** a 'cat' Label attached to them.
     """
 
 def section11():
     """
     #### Exist
-    The filter param FILTERS_OPERATIONS_EXISTS checks if an attribute exists. The following example checks if there is an item with user metadata:
+    The filter param FILTERS_OPERATIONS_EXISTS checks if an attribute exists. The following example checks if there is an Item with user Metadata:
     """
