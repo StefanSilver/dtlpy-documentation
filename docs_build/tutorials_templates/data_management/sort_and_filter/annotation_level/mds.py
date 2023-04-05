99d485a52a5e4b25a1445c1b33280f4e883f9557
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-05 18:31:17 +0300

    Update mds.py

--- docs_build/tutorials_templates/data_management/sort_and_filter/annotation_level/mds.py
+++ docs_build/tutorials_templates/data_management/sort_and_filter/annotation_level/mds.py
@@ -4,25 +4,30 @@ def section1():
     ## The Dataloop Query Language - DQL
     Using The <a href="https://dataloop.ai/docs/api-dql" target="_blank">Dataloop Query Language</a>, you may navigate through massive amounts of data. The Dataloop platform gives you different capabilities to organize your data in Datasets, folders, and versioning systems, you still need the ability to query your data. This is where our Dataloop Query Language becomes useful. When using the DQL in the SDK, additional fields such as Sort, Page, and pageSize can be defined to sort the data that is returned from the Query. Every DQL query has the following components:
     - Resource - The target Resource for the Query; the Resource can be Items or Annotations.
-    - Filter - The Filter includes Attributes and logical operators to filter Items.
-    To access the filters entity click <a href="https://sdk-docs.dataloop.ai/en/latest/entities.html#module-dtlpy.entities.filters" target="_blank">here</a>.
+    - Filter - The Filter includes Attributes and logical operators to filter Items or Annotations.
+
+    
     ## Filters
     Filters are part of the Dataset and Task Browsers, enabling you to Filter Items based on every aspect of your files. When multiple Filters are used, the relationship between them will be the AND logical operator. However, the relationship between multiple values in each Filter will be the OR logical operator.
     By using Filters, you can filter Items and get a generator of the filtered Items. The Filter entity is used to build such Filters.
-    ### Filters - Field & Value
-    Filter your Items or Annotations using the parameters in the JSON code that represent its data within our system. Access your item/annotation JSON using <code>to_json()</code>.
-    A Filter **Field** refers to the attributes you filter by. For example, "dir" would be used if you wish to filter Items by their folder/directory.
+    To access the filters entity click <a href="https://sdk-docs.dataloop.ai/en/latest/entities.html#module-dtlpy.entities.filters" target="_blank">here</a>.
     
-    A Filter **Value** refers to the input by which you want to filter. For example, "/new_folder" can be the directory/folder name where the Items you wish to filter are located.
+    ### Filters - Field & Value
+    Filter your Items or Annotations using the parameters in the JSON code that represent its data within our system. Access your annotation JSON using `to_json()`.
+    A **Filter Field** refers to the attributes you filter by. For example, "dir" would be used if you wish to filter Item Annotations by their folder/directory.
+    
+    A **Filter Value** refers to the input by which you want to filter. For example, "/new_folder" can be the directory/folder name where the Item Annotations you wish to filter are located.
     
     ### Sort - Field & Value
-    A Sort **Field** refers to the field you sort your Items/Annotations list by. For example, if you sort by filename, you will get the Item list sorted in alphabetical order by filename.
+    A **Sort Field** refers to the field you sort your Annotations list by. For example, if you sort by filename, you will get the Item list sorted in alphabetical order by filename.
     
     You can see the full list of the available fields <a href="https://dataloop.ai/docs/api-dql" target="_blank">here</a>.
-    A Sort **Value** refers to the list order direction that is either ascending or descending.
-    ### Filter Annotations
-    Filter Annotations by the Annotations' JSON fields.
-    In the example below, you will get all of the note Annotations in the Dataset sorted by the Label.
+    A **Sort Value** refers to the list's order direction, which can be either ascending or descending.
+    
+    ### Filtering Annotations
+    You can filter Annotations by the Annotations' JSON fields.
+    
+    In the example below, you will get all of the Annotations in the Dataset sorted by the Label.
     """
 
 
