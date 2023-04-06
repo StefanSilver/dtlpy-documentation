6200d8461f9dffcc62d90c611b80197a3cdf532d
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-06 21:20:40 +0300

    Update mds.py

--- docs_build/tutorials_templates/annotations_image/segmentation/mds.py
+++ docs_build/tutorials_templates/annotations_image/segmentation/mds.py
@@ -1,8 +1,15 @@
 def section1():
     """
     # Segmentation
+    [Semantic segmentation or Image Segmentation](https://en.wikipedia.org/wiki/Image_segmentation) is a computer vision technique that assigns a label or class to each pixel in an image based on the objects or features present in that region. Unlike object detection, which identifies objects and draws bounding boxes around them, semantic segmentation creates a dense pixel-level map of the scene. The goal is to understand the image's content at a high level and identify the objects and regions present, useful in applications such as autonomous driving, medical imaging, and surveillance. Techniques include convolutional neural networks (CNNs), fully convolutional networks (FCNs), and encoder-decoder architectures, which require annotated training data to learn and generate a segmentation map for new images.
+    
     ## Init Segmentation
-    Each annotation init receives the coordinates for the specific type, label, and optional attributes. A binary mask should be exactly the same dimensions as the image item, with 0 for background and 1 for the annotation.
+    
+    Init segmentation, also known as initial segmentation, is a fundamental process in image processing and computer vision. It involves dividing an image into different regions or segments based on some initial criteria or features. The segmentation process is usually a precursor step to more advanced image analysis tasks such as object detection, image recognition, and scene understanding.
+
+    The initial criteria used for segmentation can vary depending on the specific application or problem being addressed. For instance, in a medical image, the segmentation may be based on the differences in intensity between different types of tissue. In a natural scene, the segmentation may be based on color, texture, or edge information. In some cases, a combination of features may be used to achieve better segmentation results.
+    
+    Each Annotation's init receives the coordinates for the specific type, Label, and other optional attributes. A binary mask should be exactly the same dimensions as the image Item, with 0 for background and 1 for the Annotation.
 
 
     """
@@ -11,7 +18,7 @@ def section1():
 def section2():
     """
     ## Create a Semantic Segmentation Annotation
-
+    The code below creates and uploads a Semantic Segmentation Annotation for an image to the platform, by first retrieving the Item, creating a builder instance, defining a mask with a specific Label, adding the Segmentation Annotation to the builder, displaying the Annotations, and uploading the Annotations to the platform for a specific Item.
 
     """
 
@@ -19,8 +26,8 @@ def section2():
 def section3():
     """
     ## Convert Mask to Polygon
-    The Dataloop SDK includes a function to convert a semantic mask to a polygon annotation, which is often easier to edit and work with in the UI.
-    The following example filters for items with semantic mask annotations, and converts them into Polygon annotations.
+    The Dataloop Python SDK includes a function that can be used to convert a semantic mask to a Polygon Annotation, which is often easier to edit and work with in the Web UI version of Dataloop.
+    The following example filters for Items that have semantic mask Annotations, and automatically converts them into Polygon Annotations.
 
     """
 
