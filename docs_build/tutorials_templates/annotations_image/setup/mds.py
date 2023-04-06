9602345c4a1d75f0426b02af616abfca0e1e681c
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-06 13:08:57 +0300

    Update mds.py

--- docs_build/tutorials_templates/annotations_image/setup/mds.py
+++ docs_build/tutorials_templates/annotations_image/setup/mds.py
@@ -1,9 +1,10 @@
 def section1():
     """
     # Setup
-
-    This tutorial guides you through the process using the Dataloop SDK to create and upload annotations into items.
-    The tutorial includes chapters with different tools, and the last chapter includes various more advanced scripts
+    
+    This tutorial will guide you through the process using the Dataloop Python SDK to create and upload Annotations into Items. It includes chapters that teaches you how to use different tools. The last chapter includes various more advanced scripts.
+    
+    Here is the list of all that's encompassed in this tutorial section:
     - [Classification Point & Pose](../classification_point_and_pose/chapter.md)
     - [Bounding Box & Cuboid](../bounding_box_and_cuboid/chapter.md)
     - [Polygon & Polyline](../polygon_and_polyline/chapter.md)
@@ -16,7 +17,8 @@ def section1():
         - [Parenting Annotations](../advance_tutorials/chapter.md#parenting-annotations)
         - [Change Annotation's Label to a New Label](../advance_tutorials/chapter.md#change-annotations-label)
 
-
+    Before trying to run any of the scripts in this tutorial, remember that you must Log In to Dataloop, and select the Project and Dataset you will be working on. If you don't have a Project and Dataset created, [find out how to do that here](https://developers.dataloop.ai/onboarding/02_login_and_project_and_dataset_creation/). The code below shows you how to import Dataloop's Python SDK package (`dtlpy`), and how to `get` the Project and Dataset you will be working on, if you already have them created.
+    
 
     """
 
@@ -24,9 +26,9 @@ def section1():
 def section2():
     """
     ## Initiation
-    Using the annotation definitions classes you can create, edit, view and upload platform annotations. Each annotation init receives the coordinates for the specific type, label, and optional attributes.
+    Using the Annotation definition classes you can create, edit, view and upload platform Annotations. Each Annotation's `init` receives the coordinates for the specific type, Label, and other optional attributes.
 
     ## Optional Plotting
-    Before updating items with annotations, you can optionally plot the annotation you created and review it before uploading it. This applies to all annotations described in the following section.
+    Before updating Items with Annotations, you can optionally plot the Annotation you created and review it before uploading it. This applies to all Annotations described in the next section.
 
     """
