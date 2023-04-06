b83655345b2ca04e84e32d8c6bb4ed9c31a2d6a3
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-06 14:21:07 +0300

    Update mds.py

--- docs_build/tutorials_templates/annotations_image/ellipse_and_item_description/mds.py
+++ docs_build/tutorials_templates/annotations_image/ellipse_and_item_description/mds.py
@@ -1,6 +1,13 @@
 def section1():
     """
     # Ellipse and Item Description
+    Ellipse and Item Description are both concepts related to computer vision and image processing.
+
+    An Ellipse is a geometric shape that is characterized by its center point, two radii (major and minor), and an orientation angle. In computer vision and image processing, ellipses can be used to represent objects in an image or to define regions of interest.
+
+    Ellipse Annotations can be used to annotate objects that have an elliptical shape, such as eyes, wheels, or plates. In computer vision applications, ellipse Annotations can be used for object detection, tracking, and segmentation.
+
+    Item description, on the other hand, refers to the Metadata or information associated with an Item. For example, in the case of image or video, Item description can include Metadata information such as the date the image was taken, the location, the camera settings, and any additional details or context that may be relevant. Itemdescriptions can be used to organize and categorize Items, and can also provide useful information for machine learning and computer vision algorithms.
     ## Create Ellipse Annotation
 
     """
