da5f6d32db219a711670c84ee861f1a8e3ceecca
StefanSilver <58508793+StefanSilver@users.noreply.github.com>
2023-04-10 13:20:37 +0300

    Update mds.py

--- docs_build/tutorials_templates/recipe_and_ontology/concepts/mds.py
+++ docs_build/tutorials_templates/recipe_and_ontology/concepts/mds.py
@@ -1,11 +1,32 @@
 def section1():
     """
-    ## Recipe and Ontology Concepts
-    The Dataloop Recipe & Ontology concepts are detailed in our documentation. In short:
-    * Ontology - an entity that contains labels and attributes. An attribute is linked to a label
-    * Recipe - An entity that ties an ontology with labeling instructions
-        * Linked with an ontology
-        * Labeling tools (e.g. box, polygon etc)
-        * Optional PDF instructions
-        * And more…
+    # Recipe and Ontology
+    
+    Dataloop’s Recipe builder provides data scientists with the flexibility of creating the Ontology they need for supporting their data-centric approach to managing data annotation Tasks.
+    
+    A Recipe is composed of two main components: the Ontology and instructions (or data Recipe). Let’s start with the data Recipe.
+
+    The data Recipe is the human-readable instruction that explains to the data Annotator how to annotate the data. The more expert your AI becomes, the more important your Recipe will become.
+
+    A Recipe has 3 main roles:
+    - To create a frictionless labeling instruction medium.
+    - To enable automatic UI simplifications and adjustments.
+    - To enable automatic quality checking.
+    
+    A "Recipe" refers to a set of instructions or rules that define how data should be processed, labeled, or analyzed within a Project. Recipes can be thought of as templates or workflows that provide a standardized way of working with data and can help to streamline the process of generating labeled Datasets for machine learning and other applications. Linked with an Ontology, the Recipe adds labeling instructions and settings, such as labeling tools to be used, mapping of tools to specific labels/Attributes, PDF instructions file, and more. Recipes in the Dataloop system can be customized and adapted to fit a wide range of use cases and data types.
+    
+    ## Ontology
+    
+    The Ontology is a part of the Recipe. The Ontology of a Dataset in Dataloop is the building block of your model and will help you define, for example, the object detection your trained Model provides. In its most basic form it's a Label map that comes with more powerful capabilities. The Ontology is a part of the Recipe containing the Labels and Attributes, 2 important components that are used in your Project:
+
+    - Labels (like classes) - are the names you use to classify your Annotations.
+    - Attributes - allow additional independent degrees of freedom while building a world definition.
+    
+    
+    If you want to find out more about Recipe and Ontology in Dataloop, use the following reources:
+    - [The Ultimate Guide to Data Recipes](https://dataloop.ai/blog/data-recipes/).
+    - [Taxonomy overview - Dataloop's Recipe and Ontology Documentation](https://dataloop.ai/docs/taxonomy-overview).
+    - [Recipe and Ontology Python SDK Cheat-sheet](https://dataloop.ai/docs/sdk-cheatsheet#:~:text=MyValue%27%0Aitem.update()-,Recipe%20And%20Ontology,-%E2%80%A2%20Add%20a%20label).
+    - [Dataloop's Python SDK Detailed Command Documentation](https://sdk-docs.dataloop.ai/en/latest/entities.html#dtlpy.entities.recipe.Recipe).
+    
     """
